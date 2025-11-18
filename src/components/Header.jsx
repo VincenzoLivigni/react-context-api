fdb10f1dbe67428297f068ef636d869aafa1c1fd
VincenzoLivigni <enzolivigni13@gmail.com>
2025-11-18 14:13:43 +0100

    add Navbar component with a static button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,9 @@
-import { NavLink } from "react-router-dom"
+import Navbar from "./Navbar"
 
-const menu = [
-    {
-        id: 1,
-        namePage: "HomePage",
-        link: "/"
-    },
-    {
-        id: 2,
-        namePage: "Abouts",
-        link: "/abouts"
-    },
-    {
-        id: 3,
-        namePage: "Products",
-        link: "/products"
-    }
-]
-
-export default function footer() {
+export default function navbar() {
     return (
         <header>
-            <nav className="navbar d-flex align-items-center">
-                <div className='container'>
-                    <ul className="nav">
-                        {
-                            menu.map((item) => (
-                                <li key={item.id} className="nav-item">
-                                    <NavLink className="nav-link" aria-current="page" to={item.link}>{item.namePage}</NavLink>
-                                </li>
-                            ))
-                        }
-                    </ul>
-                </div>
-            </nav>
+            <Navbar />
         </header>
     )
 }
